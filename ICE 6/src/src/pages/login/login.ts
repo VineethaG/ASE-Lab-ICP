48e5b60a604b9556d02e772b8abe7eed1f178038
VineethaG <vineetha.gummadi@gmail.com>
2018-10-01 01:50:44 -0500

    Simple Ionic Application
    Login, Registration & Home page with tabs template

---
+++ ICE 6/src/src/pages/login/login.ts
@@ -0,0 +1,54 @@
+import { Component,ViewChild } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {HomePage} from '../home/home'
+import {RegisterPage} from '../register/register'
+import {TabsPage} from '../tabs/tabs'
+/**
+ * Generated class for the LoginPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html',
+})
+export class LoginPage {
+@ViewChild('username') uname;
+@ViewChild('password') pwd;
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+  
+// Navigate to Home Page  
+signIn(){
+var validate = true
+
+if(!(this.uname.value == "admin" && this.pwd.value == "admin"))
+{
+alert("Invalid Credentials")
+validate= false
+}
+
+if(this.uname.value == "" || this.pwd.value == ""){
+alert("Please enter the credentials")
+validate= false
+}
+
+
+if (validate == true){
+this.navCtrl.push(TabsPage)
+}
+}
+
+// Navigate to Register Page  
+register(){
+this.navCtrl.push(RegisterPage)
+}
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad LoginPage');
+  }
+
+}
---
+++ ICE 6/src/src/pages/register/register.ts
@@ -0,0 +1,61 @@
+import { Component, ViewChild } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {HomePage} from '../home/home'
+import {LoginPage} from '../login/login'
+import {TabsPage} from '../tabs/tabs'
+
+/**
+ * Generated class for the RegisterPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-register',
+  templateUrl: 'register.html',
+})
+export class RegisterPage {
+@ViewChild('username') emailId;
+@ViewChild('fname') firstName;
+@ViewChild('lname') lastName;
+@ViewChild('password') pwd;
+@ViewChild('confirmPwd') confirmPwd;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad RegisterPage');
+  }
+  
+register(){
+var validate = true
+
+if(this.emailId.value == "" || this.firstName.value == "" || this.lastName.value ==""|| this.pwd.value == "" || this.confirmPwd.value == "") 
+{
+alert("Fields should not be empty" )
+validate = false;
+}
+
+if(!(this.pwd.value == this.confirmPwd.value)) 
+{
+alert("Password Should Match" )
+validate = false;
+}
+
+if (validate == true){
+alert("Succesfully Registered")
+this.navCtrl.push(LoginPage)
+}
+
+}
+
+
+
+login(){
+this.navCtrl.push(LoginPage)
+}
+
+}
